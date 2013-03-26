fab9194e5c991a2b0795f32edad4d3879fb94a82
maphysics <maphysics@ubuntu.(none)>
2013-03-26 11:52:58 -0500

    tracer for now

--- src/jvm/storm/starter/TracerHook.java
+++ src/jvm/storm/starter/TracerHook.java
@@ -1,19 +1,24 @@
 package storm.starter;
 
 import backtype.storm.hooks.BaseTaskHook;
+import backtype.storm.hooks.info.BoltExecuteInfo;
 import backtype.storm.hooks.info.EmitInfo;
+import backtype.storm.hooks.info.SpoutAckInfo;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.generated.GlobalStreamId;
 import backtype.storm.generated.Grouping;
 import backtype.storm.tuple.Fields;
 import backtype.storm.generated.Bolt;
+import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import com.esotericsoftware.minlog.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
-
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,136 +28,127 @@ import java.util.HashMap;
  * To change this template use File | Settings | File Templates.
  */
 public class TracerHook extends BaseTaskHook {
-    public static Logger LOG = LoggerFactory.getLogger(TracerHook.class);
 
-    private java.util.Map<GlobalStreamId,Grouping> sources;
-    private String componentID;
-    private Fields outputFields;
-    private java.util.Map<java.lang.String,java.util.Map<java.lang.String,Grouping>> targets;
-    private int _traceIndex;
-    private String logString = "";
-    private HashMap _traceInfo = new HashMap();
-    private int traceinfoInt = 0;
 
-    @Override
-    public void prepare(java.util.Map conf, TopologyContext context){
-        componentID = context.getThisComponentId();
-        targets = context.getThisTargets();
-        if (componentID != "__acker"){
-            sources = context.getThisSources();
-            outputFields = context.getThisOutputFields("default");
-            targets = context.getThisTargets();
+    private HashMap allInfo = new HashMap();
+
+    private String logString = "\n";
+    private String traceTrail;
+    private String traceTrailNew;
+    int traceinfoInt = -1;
+    HashMap traceInfoNew = new HashMap();
+
+    public void getBasicBoltExecuteInfo(BoltExecuteInfo info){
+        allInfo.put("tuple", info.tuple);
+        allInfo.put("trace", info.tuple.getBooleanByField("_trace"));
+        allInfo.put("traceInfo", info.tuple.getValueByField("traceInfo"));
+        HashMap traceInfo = (HashMap) allInfo.get("traceInfo");
+
+        allInfo.put("traceId", traceInfo.get("traceId"));
+        allInfo.put("traceTrail", traceInfo.get("traceTrail"));
+    }
+
+    public void getBoltExecuteInfo(BoltExecuteInfo info){
+        allInfo.put("values", info.tuple.getValues());
+    }
+
+    public void getPrepareInfo(java.util.Map conf, TopologyContext context, String streamName){
+        allInfo.put("componentID", context.getThisComponentId());
+        if (allInfo.get("componentID") != "__acker"){
+            allInfo.put("sources", context.getThisSources());
+            allInfo.put("outputFields", context.getThisOutputFields(streamName));
+            allInfo.put("targets", context.getThisTargets());
         }
     }
 
-    @Override
-    public void emit(EmitInfo info){
-        try{
-            //Get the location of _trace in the output fields, this assumes it was the same place in the input fields
-            _traceIndex = outputFields.fieldIndex("_trace");
-        }
-        catch(Exception name){
-            _traceIndex = -1;
-        }
-        try{
-            traceinfoInt = outputFields.fieldIndex("traceInfo");
-            _traceInfo = (HashMap) info.values.get(traceinfoInt);
-        }
-        catch(Exception name){
-            traceinfoInt = -1;
-            _traceInfo = new HashMap();
-        }
-
-        if (_traceIndex != -1 ){
-            String logString = "\n componentID   sources   outputFields   targets   outputValues \n ";
-
-
-            logString = logString.concat(componentID + " \t ");
-            if (componentID != "__acker"){
-                logString = logString.concat(sources + " \t ");
-                logString = logString.concat(outputFields + " \t ");
-                logString = logString.concat(targets + " \t ");
+    public void messageMake(Vector<String> allInfoKeys){
+        Iterator it = allInfo.keySet().iterator();
+        while(it.hasNext()) {
+            String key = (String) it.next();
+            if (allInfoKeys.contains(key)){
+                Object val = (Object) allInfo.get(key);
+                logString = logString.concat(key + ": " + val + "\t");
             }
+        }
+    }
 
-            //__system is a system stream emitted by bolts and spouts and it only emits startup events
-            //ignore these as they don't have the format I want
-            //FIXME! figure out how to handle _trace fields not being where I think they are
-            logString = logString.concat(info.values + " \t ");
+    public void output(Vector<String> allInfoKeys){
+        Logger LOG = LoggerFactory.getLogger(TracerHook.class);
+        messageMake(allInfoKeys);
+        LOG.info(logString);
+    }
 
-            if (info.values.get(0) !=  new Values("startup").get(0) && info.values.size() > _traceIndex){
-                //this checks that the tuple does has a bool at the correct index
-                //metrcis tuple for instance don't
-                //Also think there is a tuple sent out from the spout after running for awhile that messes with things
-                try{
-                    if (info.values.get(_traceIndex) instanceof Boolean){
-                        Boolean fail = (Boolean) info.values.get(_traceIndex);
-                        //Only produce logString when input _trace == true
-                        if (fail == true){
-                            LOG.info(logString);                    }
-                    }
-                }
-                catch(Exception name){
-                    logString = "The tuple dies with the Exception: " + name + "\n" + logString;
-                    logString = logString.concat("\n _traceIndex: " + _traceIndex);
-                    logString = logString.concat("\n info.values.size(): " + info.values.size());
-                    LOG.info(logString);
-                }
+    public void updateTraceTrail(String component){
+        if ((Boolean) allInfo.get("trace") == true){
+            Tuple tuple = (Tuple) allInfo.get("tuple");
+            HashMap tInfo = (HashMap) tuple.getValueByField("traceInfo");
+            String tTrail = (String) tInfo.get("traceTrail");
+            tTrail = component + "(" + tTrail + ")";
+            tInfo.put("traceTrail", tTrail);
+            allInfo.put("traceTrail", tTrail);
+            allInfo.put("newTraceInfo", tInfo);
+        }
+    }
+
+    public void updateEmitTraceTrail(String component, EmitInfo info){
+        Boolean trace = (Boolean) allInfo.get("trace");
+        Tuple tuple = (Tuple) allInfo.get("tuple");
+
+        System.out.println("trace: " + trace + " tuple:" + tuple);
+
+
 //        try{
-//            //Get the location of _trace in the output fields, this assumes it was the same place in the input fields
-//            _traceIndex = outputFields.fieldIndex("_trace");
-//            Boolean trace = (Boolean) info.values.get(_traceIndex);
-//        }
-//        catch(Exception name){
-//            _traceIndex = -1;
-//        }
-////        try{
-////            traceinfoInt = outputFields.fieldIndex("traceInfo");
-////            _traceInfo = (HashMap) info.values.get(traceinfoInt);
-////        }
-////        catch(Exception name){
-////            traceinfoInt = -1;
-////            _traceInfo = new HashMap();
-////        }
-//        if (_traceIndex != -1 ){
+//            if ((Boolean) allInfo.get("trace") == true){
+//                Tuple tuple = (Tuple) allInfo.get("tuple");
+//                Integer traceInfoIndex = tuple.fieldIndex("traceInfo");
+//                System.out.println("from component: " + component+ " traceInfoIndex: " + traceInfoIndex +
+//                        " and info.value.size() " + info.values.size() + " and tuple.size() " + tuple.size() +
+//                        " values are: " + info.values);
+//                if (info.values.get(traceInfoIndex) != null){
+//                    if (info.values.size() - tuple.size() == 1){
+//                        info.values.remove(traceInfoIndex);
+//                        info.values.add(traceInfoIndex, allInfo.get("newTraceInfo"));
+//                    }
+//                    else{
+//                        Integer delta = info.values.size() - tuple.size();
 //
-//            logString = logString.concat("\ncomponentID: \t" + componentID + " \t ");
-////            if (componentID != "__acker"){
-////                logString = logString.concat("\nsources: \t" + sources + " \t ");
-////                logString = logString.concat("\noutputFields: \t" +outputFields + " \t ");
-////                logString = logString.concat("\ntargets: \t" + targets + " \t ");
-////            }
-//
-//            //__system is a system stream emitted by bolts and spouts and it only emits startup events
-//            //ignore these as they don't have the format I want
-//            //FIXME! figure out how to handle _trace fields not being where I think they are
-//
-//
-//            if (info.values.get(0) !=  new Values("startup").get(0)){
-//                //this checks that the tuple does has a bool at the correct index
-//                //metrcis tuple for instance don't
-//                //Also think there is a tuple sent out from the spout after running for awhile that messes with things
-//
-//                if (info.values.get(_traceIndex) instanceof Boolean){
-//                    Boolean trace = (Boolean) info.values.get(_traceIndex);
-//                    //Only produce logString when input _trace == true
-//                    if (trace == true){
-////                        String _traceTrail = (String) _traceInfo.get("traceTrail");
-////                        _traceTrail = componentID + "(" + _traceTrail + ")";
-////                        _traceInfo.put("traceTrail", _traceTrail);
-////                        if (traceinfoInt != -1){
-////                            info.values.remove(traceinfoInt);
-////                            info.values.add(traceinfoInt, _traceInfo);
-////                            logString = logString.concat("\nupdateTrail: \t Yes");
-////                        }
-////                        else{
-////                            logString = logString.concat("\nupdateTrail: \t No");
-////                        }
-//                        logString = logString.concat("\nvalues: \t" + info.values + " \t ");
-//                        LOG.info(logString);
 //                    }
 //                }
+//                else{
+//                    System.out.print("traceInfoIndex points to a null value" + traceInfoIndex);
+//                }
 //            }
-            }
+//        }
+//        catch(Exception name){
+//            System.out.println(name + ": from updateEmitTraceTrail probably fine?");
+//        }
+    }
+
+    @Override
+    public void prepare(java.util.Map conf, TopologyContext context){
+        getPrepareInfo(conf, context, "default");
+    }
+
+    public void emit(EmitInfo info){
+        //May duplicate trail names, not sure
+        updateEmitTraceTrail((String) allInfo.get("componentID"), info);
+    }
+
+    public void boltExecute(BoltExecuteInfo info){
+        getBasicBoltExecuteInfo(info);
+        getBoltExecuteInfo(info);
+        updateTraceTrail((String) allInfo.get("componentID"));
+
+        if ((Boolean) allInfo.get("trace") == true){
+            Vector<String> allInfoKeys = new Vector();
+            allInfoKeys.add("componentID");
+            allInfoKeys.add("traceId");
+            allInfoKeys.add("sources");
+            allInfoKeys.add("outputFields");
+            allInfoKeys.add("targets");
+            allInfoKeys.add("traceTrail");
+            allInfoKeys.add("values");
+            output(allInfoKeys);
         }
     }
 }
