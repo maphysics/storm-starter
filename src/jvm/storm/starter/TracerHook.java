df85b50be65de22203379a40275ddc1207ea664b
maphysics <maphysics@ubuntu.(none)>
2013-03-27 15:26:18 -0500

    breaking out methods

--- src/jvm/storm/starter/TracerHook.java
+++ src/jvm/storm/starter/TracerHook.java
@@ -32,23 +32,33 @@ public class TracerHook extends BaseTaskHook {
 
     private HashMap allInfo = new HashMap();
 
-    private String logString = "\n";
     private String traceTrail;
     private String traceTrailNew;
     int traceinfoInt = -1;
     HashMap traceInfoNew = new HashMap();
 
+//    public Boolean isTracerRecord(Tuple tuple){
+//        Object trace = (Object) tuple.getValueByField("_trace");
+//        if (trace ==){
+//            return true;
+//        }
+//        else{
+//            return false
+//        }
+//    }
+
     public void getBasicBoltExecuteInfo(BoltExecuteInfo info){
         allInfo.put("tuple", info.tuple);
-        allInfo.put("trace", info.tuple.getBooleanByField("_trace"));
-        allInfo.put("traceInfo", info.tuple.getValueByField("traceInfo"));
-        HashMap traceInfo = (HashMap) allInfo.get("traceInfo");
+        allInfo.put("trace", info.tuple.getValueByField("_trace"));
+        HashMap traceInfo = (HashMap) allInfo.get("_trace");
 
-        allInfo.put("traceId", traceInfo.get("traceId"));
+        allInfo.put("traceID", traceInfo.get("traceID"));
         allInfo.put("traceTrail", traceInfo.get("traceTrail"));
     }
 
     public void getBoltExecuteInfo(BoltExecuteInfo info){
+        getBasicBoltExecuteInfo(info);
+        allInfo.put("fields", info.tuple.getFields());
         allInfo.put("values", info.tuple.getValues());
     }
 
@@ -59,9 +69,15 @@ public class TracerHook extends BaseTaskHook {
             allInfo.put("outputFields", context.getThisOutputFields(streamName));
             allInfo.put("targets", context.getThisTargets());
         }
+        else{
+            allInfo.put("sources", "from __acker");
+            allInfo.put("outputfields", "from __acker");
+            allInfo.put("targets", "from __acker");
+        }
     }
 
-    public void messageMake(Vector<String> allInfoKeys){
+    public String messageMake(Vector<String> allInfoKeys){
+        String logString = "";
         Iterator it = allInfo.keySet().iterator();
         while(it.hasNext()) {
             String key = (String) it.next();
@@ -70,31 +86,38 @@ public class TracerHook extends BaseTaskHook {
                 logString = logString.concat(key + ": " + val + "\t");
             }
         }
+        return logString;
     }
 
-    public void output(Vector<String> allInfoKeys){
+    public void output(String logstring){
         Logger LOG = LoggerFactory.getLogger(TracerHook.class);
-        messageMake(allInfoKeys);
-        LOG.info(logString);
+        LOG.info(logstring);
     }
 
     public void updateTraceTrail(String component){
-        if ((Boolean) allInfo.get("trace") == true){
-            Tuple tuple = (Tuple) allInfo.get("tuple");
-            HashMap tInfo = (HashMap) tuple.getValueByField("traceInfo");
-            String tTrail = (String) tInfo.get("traceTrail");
-            tTrail = component + "(" + tTrail + ")";
-            tInfo.put("traceTrail", tTrail);
-            allInfo.put("traceTrail", tTrail);
-            allInfo.put("newTraceInfo", tInfo);
-        }
+        output("from updateTraceTrail" + allInfo.keySet());
+//        if (allInfo.keySet().contains("tuple")){
+//            Tuple tuple = (Tuple) allInfo.get("tuple");
+//        }
+//        else{
+//            System.out.println("from updateTraceTrail, allInfo does not contain \"tuple\"");
+//        }
+//        HashMap tInfo = (HashMap) tuple.getValueByField("traceInfo");
+//        String tTrail = (String) tInfo.get("traceTrail");
+//        tTrail = component + "(" + tTrail + ")";
+//        tInfo.put("traceTrail", tTrail);
+//        allInfo.put("traceTrail", tTrail);
+//        allInfo.put("newTraceInfo", tInfo);
     }
 
     public void updateEmitTraceTrail(String component, EmitInfo info){
-        Boolean trace = (Boolean) allInfo.get("trace");
-        Tuple tuple = (Tuple) allInfo.get("tuple");
-
-        System.out.println("trace: " + trace + " tuple:" + tuple);
+        output("from updateemitTraceTrail" + allInfo.keySet());
+//        updateTraceTrail((String) allInfo.get("componentID"));
+//        HashMap trace = (HashMap) allInfo.get("trace");
+//        Tuple tuple = (Tuple) allInfo.get("tuple");
+//        Integer traceIndex = tuple.fieldIndex("_trace");
+//
+//        System.out.println("trace: " + trace + " tuple:" + tuple);
 
 
 //        try{
@@ -127,29 +150,57 @@ public class TracerHook extends BaseTaskHook {
     @Override
     public void prepare(java.util.Map conf, TopologyContext context){
         getPrepareInfo(conf, context, "default");
+        Vector elements = new Vector();
+        elements.add("componentID");
+        elements.add("sources");
+        elements.add("outputFields");
+        elements.add("targets");
+        output(messageMake(elements));
     }
 
     public void emit(EmitInfo info){
         //May duplicate trail names, not sure
-        updateEmitTraceTrail((String) allInfo.get("componentID"), info);
+        if (allInfo.get("componentID") != "__system" && allInfo.get("componentID") != "spout"){
+            output("from emit componentID: " + allInfo.get("componentID") + " values: " + allInfo.get("values") +
+                    " trace: " + allInfo.get("trace"));
+            if (allInfo.get("trace") != new HashMap() && allInfo.get("trace") != null){
+                output("from emit trace inner: " + allInfo.get("trace"));
+                output("from updateemitTraceTrail" + allInfo.keySet());
+//                updateEmitTraceTrail((String) allInfo.get("componentID"), info);
+                Vector elements = new Vector();
+                elements.add("traceID");
+                elements.add("traceTrail");
+                output(messageMake(elements));
+            }
+        }
     }
 
     public void boltExecute(BoltExecuteInfo info){
-        getBasicBoltExecuteInfo(info);
-        getBoltExecuteInfo(info);
-        updateTraceTrail((String) allInfo.get("componentID"));
-
-        if ((Boolean) allInfo.get("trace") == true){
-            Vector<String> allInfoKeys = new Vector();
-            allInfoKeys.add("componentID");
-            allInfoKeys.add("traceId");
-            allInfoKeys.add("sources");
-            allInfoKeys.add("outputFields");
-            allInfoKeys.add("targets");
-            allInfoKeys.add("traceTrail");
-            allInfoKeys.add("values");
-            output(allInfoKeys);
+        if (allInfo.get("componentID") != "__system"){
+            output("from boltExecute _trace: " + info.tuple.getValueByField("_trace"));
+            if (info.tuple.getValueByField("_trace") != new HashMap() && info.tuple.getValueByField("_trace") != null){
+                getBoltExecuteInfo(info);
+                Vector elements = new Vector();
+                elements.add("traceID");
+                elements.add("fields");
+                elements.add("values");
+                output(messageMake(elements));
+            }
         }
     }
+
+//    public void spoutAck(SpoutAckInfo info){
+//        if (allInfo.get("componentID") != "__system"){
+//            output("from boltExecute _trace: " + info.tuple.getValueByField("_trace"));
+//            if (info.tuple.getValueByField("_trace") != new HashMap() && info.tuple.getValueByField("_trace") != null){
+//                getBoltExecuteInfo(info);
+//                Vector elements = new Vector();
+//                elements.add("traceID");
+//                elements.add("fields");
+//                elements.add("values");
+//                output(messageMake(elements));
+//            }
+//        }
+//    }
 }
 
